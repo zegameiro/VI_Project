24a0319fd4fddb5003b5282ddf712f515f429535
zegameiro <jose.mcgameiro@ua.pt>
2024-11-24 00:05:26 +0000

    Finished HelpPopOver implementation

--- src/components/HelpPopOver.jsx
+++ src/components/HelpPopOver.jsx
@@ -3,18 +3,33 @@ import {
   PopoverTrigger,
   PopoverContent,
   Button,
+  Divider
 } from "@nextui-org/react";
+import { LuHelpCircle } from "react-icons/lu";
 
-const HelpPopHover = ({ information }) => {
+const HelpPopHover = ({ information, placement }) => {
   return (
-    <Popover placement="down">
+    <Popover className="dark text-white max-w-md" placement={placement}>
       <PopoverTrigger>
-        <Button color="primary">Help</Button>
+        <Button color="primary" className="flex flex-row items-center gap-2">Help <LuHelpCircle className="text-lg"/> </Button>
       </PopoverTrigger>
       <PopoverContent>
         <div className="px-1 py-2">
-          <div className="text-small font-bold">Popover Content</div>
-          <div className="text-tiny">This is the popover content</div>
+          {Object.entries(information).map(([key, value], index) => (
+            <div key={index} className="flex flex-col">
+              <span className="text-lg font-bold pb-2">{key}</span>
+              {Array.isArray(value) ? (
+                <ul className="list-disc ml-4 text-[#ccc]">
+                  {value.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+              ) : (
+                <span className="text-[#ccc]">{value}</span>
+              )}
+              <Divider className="my-3" />
+            </div>
+          ))}
         </div>
       </PopoverContent>
     </Popover>
