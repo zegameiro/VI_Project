f82d1d8ab8f237194f154b816204b959b98916e7
Dan1m4d <daniel.madureira@ua.pt>
2024-11-27 02:00:46 +0000

    try to fix deploymen problem

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   server: {
     port: 3000
   }
